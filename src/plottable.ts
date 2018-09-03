c33d31acce3fa2634ba467688ba5d2d363b39543
aleku399 <aleku399@gmail.com>
2018-09-03 18:47:39 +0300

    w.i.p

---
+++ src/plottable.ts
@@ -0,0 +1,25 @@
+import * as Plottable from 'plottable';
+
+const data = [{ x: 0, y: 1 }, { x: 1, y: 2 }, { x: 2, y: 4 },
+    { x: 3, y: 8 }, { x: 4, y: 12 }, { x: 5, y: 16 }];
+
+const barChart = () => {
+    const xScale = new Plottable.Scales.Linear();
+    const yScale = new Plottable.Scales.Linear();
+    const xAxis = new Plottable.Axes.Numeric(xScale, 'bottom');
+    const yAxis = new Plottable.Axes.Numeric(yScale, 'left');
+
+    const plot = new Plottable.Plots.Bar()
+    .x(d => d.x, xScale)
+    .y(d => d.y * 20, yScale)
+    .addDataset(new Plottable.Dataset(data))
+    .animated(true)
+    .renderTo('#barChart');
+
+    // const chart = new Plottable.Components.Table([
+    //                     [yAxis, plot],
+    //                     [null, xAxis]
+    //                     ]);
+    // chart.renderTo('#barChart');
+};
+barChart();
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
     devtool: 'inline-source-map',
-    entry: './src/index.ts',
+    entry: './src/plottable.ts',
     output: {
       filename: 'bundle.js',
       path: path.resolve(__dirname, './dist')
